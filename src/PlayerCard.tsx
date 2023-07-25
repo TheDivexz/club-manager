95439dd1817be776b3372689cdf90948edd892ea
Dhruv Tailor <dhruv61998@gmail.com>
2023-07-25 13:30:13 -0500

    Simplified and Detailed view

--- src/PlayerCard.tsx
+++ src/PlayerCard.tsx
@@ -3,35 +3,55 @@ import { playerObject } from "./playerObject";
 import './styles/player_card.css'
 
 interface playerObjectProps {
-    playerInfo: playerObject;
+    playerInfo: playerObject,
+    detailed_view: boolean
 }
 
-const PlayerCard: React.FC<playerObjectProps> = ({playerInfo}) => {
+const PlayerCard: React.FC<playerObjectProps> = ({playerInfo,detailed_view}) => {
     const [attack,setAttack] = useState(0.0);
     const [defense,setDefense] = useState(0.0);
     const [speed,setSpeed] = useState(0.0);
     const [accuracy,setAccuracy] = useState(0.0);
     const [stamina,setStamina] = useState(0.0);
+    const [averageScore,setAverageScore] = useState(0)
 
     // Converts the stats from 1-255 to 1-100
     useEffect(() => {
-        setAttack(Math.round(playerInfo.attack/255.0*100));
-        setDefense(Math.round(playerInfo.defense/255.0*100));
-        setSpeed(Math.round(playerInfo.speed/255.0*100));
-        setAccuracy(Math.round(playerInfo.accuracy/255.0*100));
-        setStamina(Math.round(playerInfo.stamina/255.0*100));
+        let attack_val = Math.round(playerInfo.attack/255.0*100)
+        let defense_val = Math.round(playerInfo.defense/255.0*100)
+        let speed_val = Math.round(playerInfo.speed/255.0*100)
+        let acc_val = Math.round(playerInfo.accuracy/255.0*100)
+        let stamina_val = Math.round(playerInfo.stamina/255.0*100)
+        let avg_val = Math.round((attack_val + defense_val + speed_val + acc_val + stamina_val) / 5)
+        setAttack(attack_val);
+        setDefense(defense_val);
+        setSpeed(speed_val);
+        setAccuracy(acc_val);
+        setStamina(stamina_val);
+        setAverageScore(avg_val)
     },[])
 
     return (
         <div className="player-card">
             <p className="player-name">{playerInfo.name}</p>
-            <div className="property-wrap">
-                <p className="property-text">Attack: {attack}</p>
-                <p className="property-text">Defense: {defense}</p>
-                <p className="property-text">Speed: {speed}</p>
-                <p className="property-text">Accuracy: {accuracy}</p>
-                <p className="property-text">Stamina: {stamina}</p>
-            </div>
+            {
+                detailed_view ? 
+                (
+                    <div className="property-wrap">
+                        <p className="property-text">Attack: {attack}</p>
+                        <p className="property-text">Defense: {defense}</p>
+                        <p className="property-text">Speed: {speed}</p>
+                        <p className="property-text">Accuracy: {accuracy}</p>
+                        <p className="property-text">Stamina: {stamina}</p>
+                    </div>
+                )
+                :
+                (
+                    <div className="property-wrap">
+                        <h1 className="property-text">{averageScore}</h1>
+                    </div>
+                ) 
+            } 
         </div>
     )
 }
--- src/squadview.tsx
+++ src/squadview.tsx
@@ -8,6 +8,7 @@ const SquadView = () => {
 
     const [squad,setSquad] = useState<playerObject[]>([]);
     const [teamName,setTeamName] = useState("");
+    const [detailedView, setDetailedView] = useState(false)
 
     async function getSquad() {
         setSquad(await invoke("get_players_in_team", {teamId: 0}));
@@ -19,6 +20,13 @@ const SquadView = () => {
     const sortBySpeed = () => { setSquad([...squad].sort((a,b) => b.speed - a.speed)) }
     const sortByAccuracy = () => { setSquad([...squad].sort((a,b) => b.accuracy - a.accuracy)) }
     const sortByStamina = () => { setSquad([...squad].sort((a,b) => b.stamina - a.stamina)) } 
+    const sortByAverage = () => {
+        setSquad([...squad].sort((a,b) => {
+            const a_avg = (a.attack + a.defense + a.speed + a.accuracy + a.stamina)/5
+            const b_avg = (b.attack + b.defense + b.speed + b.accuracy + b.stamina)/5
+            return b_avg - a_avg;
+        }))
+    }
 
     useEffect(() => {
         getSquad();
@@ -28,17 +36,23 @@ const SquadView = () => {
         <div className="background">
             <h1 className="team-name">{teamName}</h1>
             <div className="filter-bar">
+                <span>Card View: </span>
+                <button className="filter-btn" onClick={() => setDetailedView(false)}>Simplified</button>
+                <button className="filter-btn" onClick={() => setDetailedView(true)}>Detailed</button>
+            </div>
+            <div className="filter-bar">
                 <span>Filter By: </span>
                 <button className="filter-btn" onClick={sortByAttack}>Attack</button>
                 <button className="filter-btn" onClick={sortByDefense}>Defense</button>
                 <button className="filter-btn" onClick={sortBySpeed}>Speed</button>
                 <button className="filter-btn" onClick={sortByAccuracy}>Accuracy</button>
                 <button className="filter-btn" onClick={sortByStamina}>Stamina</button>
+                <button className="filter-btn" onClick={sortByAverage}>Average</button>
             </div>
             <div className="squad-view">
                 {squad.map(member => {        
                     return (
-                        <PlayerCard key={member.attack+member.defense} playerInfo={member}/>
+                        <PlayerCard key={member.attack + member.defense} playerInfo={member} detailed_view={detailedView}/>
                     )
                 })}
             </div>
