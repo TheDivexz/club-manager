7646d23e1c33559691611f0a24e699d22412d13f
Dhruv Tailor <dhruv61998@gmail.com>
2023-10-31 15:22:00 -0500

    Update PlayerStatView.tsx

--- src/squad_view/PlayerStatView.tsx
+++ src/squad_view/PlayerStatView.tsx
@@ -7,7 +7,12 @@ interface PlayerStatViewProps {
 const PlayerStatView: React.FC<PlayerStatViewProps> = ({current}) => {
     return (
         <>
-            <Progress percent={Math.round(current/255.0*100)} type="dashboard" format={() => `${Math.round(current/255.0*100)}`} size="small"/>
+            <Progress 
+                percent={Math.round(current/255.0*100)} 
+                type="dashboard" 
+                format={() => `${Math.round(current/255.0*100)}`} 
+                size="small"
+            />
         </>
     )
 }
